bf786d4ff59b13758adcba2436aa5b8ff3e9bbe8
nathaniel smith <nathanielksmith@gmail.com>
2013-01-12 01:00:46 -0500

    switch to jam

---
+++ src/frontend/jam/require.config.js
@@ -0,0 +1,103 @@
+var jam = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "version": "0.2.13",
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "underscore"
+            ],
+            "exports": "Backbone"
+        },
+        "underscore": {
+            "exports": "_"
+        }
+    }
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "underscore"
+            ],
+            "exports": "Backbone"
+        },
+        "underscore": {
+            "exports": "_"
+        }
+    }
+});
+}
+else {
+    var require = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "underscore"
+            ],
+            "exports": "Backbone"
+        },
+        "underscore": {
+            "exports": "_"
+        }
+    }
+};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
