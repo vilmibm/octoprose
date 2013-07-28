1e4693a791e576d09d99d3a99b5700a692056742
Nathaniel Smith <nathaniel@puppetlabs.com>
2013-07-28 16:41:57 -0400

    support highlighting

--- three/public/js/editor/reviewer.js
+++ three/public/js/editor/reviewer.js
@@ -44,8 +44,65 @@ for (var cix = 0; cix < mdHTML.length; cix++) {
 
 $root.html(newMdHTML);
 
+// selecting
+var Ranges = function() {
+    this._ranges = [];
+    this._callbacks = {
+        push: []
+    };
+};
 
+Ranges.prototype.on = function(e, callback) {
+    this._callbacks[e].push(callback);
+    return this;
+}
 
+Ranges.prototype.emit = function(e, data) {
+    for (var i = 0; i < this._callbacks[e].length; i++) {
+        this._callbacks[e][i].call(this, data);
+    }
+    return this;
+};
 
+Ranges.prototype.push = function(rangeTuple) {
+    this._ranges.push(rangeTuple);
+    this.emit('push', rangeTuple);
+    return this;
+};
 
+var ranges = new Ranges;
+var sel = window.getSelection(); // this function returns a singleton reference.
 
+var mouseUpHandler = function(e) {
+    var selectedText = sel.toString();
+    if (selectedText.length === 0) {
+        console.log('Empty selection');
+        return;
+    }
+
+    console.log('Selected text:', selectedText);
+
+    var anchorIdx = Number(sel.anchorNode.parentElement.dataset['idx']);
+    var focusIdx  = Number(sel.focusNode.parentElement.dataset['idx' ]);
+
+    ranges.push([anchorIdx, focusIdx]);
+
+    sel.collapseToStart(); // clear selection
+};
+
+$root.on('mouseup', mouseUpHandler);
+
+var newRangeHandler = function(rangeTuple) {
+    console.log("New range:", rangeTuple);
+    var LIDX = Math.min.apply(Math, rangeTuple);
+    var RIDX = Math.max.apply(Math, rangeTuple);
+
+    for (var idx = LIDX; idx <= RIDX; idx++) {
+        console.log(idx);
+        $('span[data-idx="'+idx+'"]').css('backgroundColor', 'red');
+        // ask for new comment
+        // if comment is made, add to comments collection
+    }
+};
+
+ranges.on('push', newRangeHandler);
