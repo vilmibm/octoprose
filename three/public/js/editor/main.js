e1f2acff95599724d12f4cedc1e98e105a52a105
Nathaniel Smith <nathaniel@puppetlabs.com>
2013-08-04 09:35:37 -0400

    view depends on suggestions.fetch being called after its been rendered... fix that?

--- three/public/js/editor/main.js
+++ three/public/js/editor/main.js
@@ -9,7 +9,7 @@ var piece = new Piece(      rawPiece);
 var reviewer = new Reviewer(document.getElementById('reviewerContainer'), piece);
 var editor   = new Editor(  document.getElementById('editorContainer'), piece);
 
-piece.suggestions.fetch();
 reviewer.render();
+piece.suggestions.fetch();
 
 window.addEventListener('unload', piece.suggestions.save.bind(piece.suggestions));
